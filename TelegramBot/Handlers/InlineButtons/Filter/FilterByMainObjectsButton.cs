2526a26a4beeb0c4e2f63c56f7e2fb64f4d9034d
shamil <toshamilgis@gmail.com>
2024-03-16 17:16:16 +0300

    upd

--- TelegramBot/Handlers/InlineButtons/Filter/FilterByMainObjectsButton.cs
+++ TelegramBot/Handlers/InlineButtons/Filter/FilterByMainObjectsButton.cs
@@ -8,6 +8,7 @@ public class FilterByMainObjectsButton : Button
     public FilterByMainObjectsButton(Bot bot) : base(bot)
     {
         Value = "MainObjects";
+        
     }
     
     protected override async Task RunAsync(Context context)
